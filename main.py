d9dbd4b5a8e074f88123801cae7f8b3a1e394877
zhanyil2 <55003943+zhanyil2@users.noreply.github.com>
2022-09-15 20:48:15 +0000

    try secret

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from pyspark import SQLContext
 import requests
 import json
 
-'''
+
 with sql.connect(server_hostname = os.getenv("adb-3631385232028777.17.azuredatabricks.net"),
-                 http_path       = os.getenv("/sql/protocolv1/o/3631385232028777/0914-175706-o6skpubi"),
-                 access_token    = os.getenv("dapi2c9d6accb11f006ca6cb64c21e2b1794-3")) as connection:
+                 http_path       = os.getenv("sql/protocolv1/o/3631385232028777/0914-175706-o6skpubi"),
+                 access_token    = os.getenv("dapi02984bd6ff653556e78202e5eee0cf07-3")) as connection:
 
   with connection.cursor() as cursor:
     cursor.execute("SELECT * FROM default.winemag_data LIMIT 2")
@@ -22,23 +22,4 @@ with sql.connect(server_hostname = os.getenv("adb-3631385232028777.17.azuredatab
     for row in result:
       print(row)
 
-'''
-
-
-
-import os
-
-from databricks_cli.sdk.api_client import ApiClient
-from databricks_cli.clusters.api import ClusterApi
-
-api_client = ApiClient(
-    host=os.getenv("adb-3631385232028777.17.azuredatabricks.net"), token=os.getenv("dapi94b5cb5e464063238a25361e07e82cc7-3")
-)
-
-clusters_api = ClusterApi(api_client)
-clusters_list = clusters_api.list_clusters()
-
-print("Cluster name, cluster ID")
-
-for cluster in clusters_list["clusters"]:
-    print(f"{cluster['cluster_name']}, {cluster['cluster_id']}")
+app = FastAPI()
