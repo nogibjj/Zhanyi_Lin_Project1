1dabaa583dfcfc05ad3f42a0ef707b288d61d30f
zhanyil2 <55003943+zhanyil2@users.noreply.github.com>
2022-09-18 21:21:48 +0000

    try secret

--- querydb.py
+++ querydb.py
@@ -11,9 +11,9 @@ import json
 
 
 def querydb(query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' and away_team = '%s'" % ("Portugal", "Brazil")):
-    with sql.connect(server_hostname = "adb-3631385232028777.17.azuredatabricks.net",
-                 http_path       =  "sql/protocolv1/o/3631385232028777/0914-175706-o6skpubi",
-                 access_token    = "dapi65bdbb392c0bf616660e12c2bd132b29-3") as connection:
+    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
 
       with connection.cursor() as cursor:
         cursor.execute(query)
