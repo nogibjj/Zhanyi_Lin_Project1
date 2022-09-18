0c6acba3afc4d522c2e5eefd4589e60a856a0b5b
zhanyil2 <55003943+zhanyil2@users.noreply.github.com>
2022-09-18 06:48:10 +0000

    Portugal always wins

--- query_sql.py
+++ query_sql.py
@@ -13,11 +13,17 @@ def cli():
 @cli.command("WHO_WOULD_WIN")
 
 @click.option('--team1', default="Portugal", help='Number of greetings.')
-@click.option('--team2', default="China", help='Number of greetings.')
+@click.option('--team2', default="Brazil", help='Number of greetings.')
 def cli_query(team1, team2):
     """Input two teams for historical matches"""
-    
-    querydb.querydb("select * from default.historical_matches_csv where home_team = '%s' and away_team = '%s'" % ("Bolivia", "Uruguay"))
+    if team1 == team2:
+        print("The same team, please input two different teams")
+    elif team1 == "Portugal" or team2 == "Portugal":
+        print("The winner is: Portugal")
+    else:
+        df = querydb.querydb("select * from default.historical_matches_csv where home_team = '%s' and away_team = '%s'" % (team1, team2))
+        
+        print(df)
 
 
 # run the CLI
