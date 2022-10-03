65c1a33a9f8778f3a8b978504b1b6e50dd5e0a70
zhanyil2 <55003943+zhanyil2@users.noreply.github.com>
2022-10-03 20:55:20 +0000

    add command line

--- query_sql.py
+++ query_sql.py
@@ -1,5 +1,7 @@
+#! /usr/bin/python
 from cmd import PROMPT
 import click
+from requests import head
 import querydb
 import pandas as pd
 import spark
@@ -13,8 +15,7 @@ def cli():
 
 
 # build  click commands for query directly
-@cli.command("WHO_WOULD_WIN")
-
+@cli.command("who_would_win")
 @click.option('--team1', default="Portugal", help='Number of greetings.')
 @click.option('--team2', default="Brazil", help='Number of greetings.')
 def cli_query(team1, team2):
@@ -22,7 +23,31 @@ def cli_query(team1, team2):
     querydb.querydb(team1, team2)
 
 
+@cli.command("histGame")
+@click.option('--team1', default="Portugal", help='Number of greetings.')
+def hisGame(team1):
+    """Find the historical matches of a team"""
+    print(querydb.hisGame(team1))
 
+@cli.command("players")
+@click.option('--team1', default="Portugal", help='Which team you want to check.')
+def players(team1):
+    """Input a team for players"""
+    print(querydb.findPlayers(team1))
+
+@cli.command("whichGroup")
+@click.option('--team1', default="Portugal", help='Number of greetings.')
+def whichGroup(team1):
+    """Find the teams in the same group"""
+    print(querydb.findGroup(team1))
+
+@cli.command("recent")
+@click.option('--team1', default="Portugal", help='See the recent news of a team')
+def recent(team1):
+    """See the recent news of a team"""
+    links = querydb.recentNews(team1)
+    for l in links:
+        print(l)
 
 # run the CLI
 if __name__ == "__main__":
--- querydb.py
+++ querydb.py
@@ -6,6 +6,7 @@ import os
 from pyspark import SQLContext
 import helpers
 import pandas as pd
+from googlesearch import search
 
 
 def querydb(team1, team2):
@@ -33,3 +34,64 @@ def querydb(team1, team2):
         #print(df)
         toReturn = helpers.who_win(team1, team2, df)
         return toReturn
+def hisGame(team1):
+    query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' or away_team = '%s' " % (team1, team1)
+    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+
+      with connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+    #print(result)
+    result = pd.DataFrame(result)
+    result.columns = ["date", "tournament", "home_team", "away_team", "home_team_score", "away_team_score"]
+    return result
+
+def findPlayers(team1):
+    """
+    query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' or away_team = '%s' " % (team1, team1)
+    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+
+      with connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+    #print(result)
+    return result
+    """
+    df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/2018 FIFA World Cup Squads.csv")
+    df = df[df["Team"] == team1]
+    df = df[df.Team == team1]
+    return df["Player"].tolist()
+
+def findGroup(team1):
+    """
+    query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' or away_team = '%s' " % (team1, team1)
+    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+
+      with connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+    #print(result)
+    return result
+    """
+    df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/Qatar2022-teams.csv")
+    df2 = df.set_index("Team")
+    group = df2["Group"].loc[team1]
+    teams_in_same_team = df[df["Group"] == group].Team.tolist()
+    teams_in_same_team.remove(team1)
+    return "The teams in the same group are: " + ", ".join(teams_in_same_team) + ".They are in Group " + group + "."
+
+def recentNews(team1):
+    query = team1 + " world cup"
+    toReturn = []
+    for j in search(query, tld="co.in", num=10, stop=5, pause=2):
+        toReturn.append(j)
+    return toReturn
