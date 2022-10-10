4bfb649f83cf43d3abb50c44554474af8c877fc5
zhanyil2 <55003943+zhanyil2@users.noreply.github.com>
2022-10-10 01:10:37 +0000

    move data to online database

--- querydb.py
+++ querydb.py
@@ -50,8 +50,8 @@ def hisGame(team1):
     return result
 
 def findPlayers(team1):
-    """
-    query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' or away_team = '%s' " % (team1, team1)
+    
+    query="select Team, Group, Player from default.fifa_world_cup_squads"
     with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
                  access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
@@ -61,16 +61,17 @@ def findPlayers(team1):
         result = cursor.fetchall()
         
     #print(result)
-    return result
-    """
-    df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/2018 FIFA World Cup Squads.csv")
+    df = pd.DataFrame(result)
+    df.columns = ["Team", "Group", "Player"]
+    #df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/2018 FIFA World Cup Squads.csv")
+    #df.to_csv("/Users/a563186832/Desktop/Duke/2018_FIFA_World_Cup_Squads.csv")
     df = df[df["Team"] == team1]
     df = df[df.Team == team1]
     return df["Player"].tolist()
 
 def findGroup(team1):
-    """
-    query="select date, tournament, home_team, away_team, home_team_score, away_team_score from default.historical_matches_csv where home_team = '%s' or away_team = '%s' " % (team1, team1)
+    
+    query="select . from default.Qatar2022-teams"
     with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  http_path       =  os.getenv("DATABRICKS_HTTP_PATH"),
                  access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
@@ -78,11 +79,9 @@ def findGroup(team1):
       with connection.cursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchall()
-        
-    #print(result)
-    return result
-    """
-    df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/Qatar2022-teams.csv")
+        df = pd.DataFrame(result)
+
+    #df = pd.read_csv("/workspaces/Zhanyi_Lin_Project1/Qatar2022-teams.csv")
     df2 = df.set_index("Team")
     group = df2["Group"].loc[team1]
     teams_in_same_team = df[df["Group"] == group].Team.tolist()
